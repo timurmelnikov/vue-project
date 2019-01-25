7d6bb89954467c1d196b9cad362bf29ecfe4914a
Timur Melnikov <melnikovt@gmail.com>
2019-01-25 10:56:40 +0200

    Как запускается приложение

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App)
+    /* 2-й вариант. Почему-то не работает...
+    render: function (h) {
+        h(App)
+    }
+    */
 })
