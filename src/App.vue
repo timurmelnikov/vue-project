38c339721788d7bd81c54eca3d420bcab1b34878
Timur Melnikov <melnikovt@gmail.com>
2019-02-11 13:35:58 +0200

    Взаимодействие с элементом

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,20 @@
 <template>
     <div>
-        <h2>{{title}}</h2>
+        <h2 v-colored>{{title}}</h2>
     </div>
 </template>
 
 <script>
-
     export default {
         data() {
             return {
                 title: 'Hello I am a Vue!'
             }
         }
-
     }
 </script>
 
 <style scoped>
-
 </style>
 
 
