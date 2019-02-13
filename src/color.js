cb66b2477e4fec62ef12a5795b066f869e8548dd
Timur Melnikov <melnikovt@gmail.com>
2019-02-13 12:14:54 +0200

    Аргументы в директивах

--- src/color.js
+++ src/color.js
@@ -1,6 +1,7 @@
 export default {
     bind(el, bindings, vnode) {
         //el.style.color = 'blue'
-        el.style.color = bindings.value
+        const arg = bindings.arg
+        el.style[arg] = bindings.value
     }
 }
