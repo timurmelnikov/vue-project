807e3257a17430149082850dc0caef5633b92923
Timur Melnikov <melnikovt@gmail.com>
2019-02-13 14:34:22 +0200

    Локальная регистрация

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
     <div>
         <h2 v-colored:background.font="'red'">{{title}}</h2>
         <h2 v-colored:color.delay="'blue'">{{title}}</h2>
+        <h2 v-font>Local font directive</h2>
     </div>
 </template>
 
@@ -10,6 +11,13 @@
         data() {
             return {
                 title: 'Hello I am a Vue!'
+            }
+        },
+        directives: {
+            font: {
+                bind(el, bindings, vnode) {
+                    el.style.fontSize = '100px'
+                }
             }
         }
     }
