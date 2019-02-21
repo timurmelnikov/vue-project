6174594c58081d2ddaf29c0e427288acd0e5bbbb
Timur Melnikov <melnikovt@gmail.com>
2019-02-21 13:29:32 +0200

    Визуальное отображение ошибок

--- src/App.vue
+++ src/App.vue
@@ -4,39 +4,37 @@
             <div class="form-group">
                 <label for="email">Email</label>
                 <input
+                        :class="{'is-invalid': $v.email.$error}"
+                        @blur="$v.email.$touch()"
                         class="form-control"
                         id="email"
                         type="email"
-                        @blur="$v.email.$touch()"
                         v-model="email"
                 >
+                <div class="invalid-feedback" v-if="!$v.email.required">Поле email обязательно</div>
+                <div class="invalid-feedback" v-if="!$v.email.email">Поле email, должно содержать email</div>
             </div>
             <pre>{{$v.email}}</pre>
-
         </form>
-
     </div>
 
 </template>
 
 <script>
-    import { required, email} from 'vuelidate/lib/validators'
+    import {required, email} from 'vuelidate/lib/validators'
     export default {
-        data(){
-            return{
-                email:''
-
+        data() {
+            return {
+                email: ''
             }
         },
-        validations:{
-            email:{
+        validations: {
+            email: {
                 required: required, //Можно писать так - required
                 email
             }
         }
     }
-
-
 </script>
 
 <style scoped>
