57d0b8633c5e41a14dd4c3c843b9c269874cd19c
Timur Melnikov <melnikovt@gmail.com>
2019-02-25 10:18:28 +0200

    Установка vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
-Vue.use(Vuelidate)
+Vue.use(Vuelidate, VueRouter)
 
 new Vue({
     el: '#app',
