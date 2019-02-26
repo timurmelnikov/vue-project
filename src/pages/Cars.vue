a3ec8cdada0ba33c1f3e22709720a64651c4c511
Timur Melnikov <melnikovt@gmail.com>
2019-02-26 11:27:20 +0200

    Динамические роуты

--- src/pages/Cars.vue
+++ src/pages/Cars.vue
@@ -1,5 +1,18 @@
 <template>
-    <h1>Cars page</h1>
+    <div>
+        <h1>Cars page</h1>
+
+        <ul>
+            <router-link
+                    tag="li"
+                    v-for="car in 5"
+                    :to="'/car/'+car"
+                    :key="car"
+            >
+                <a>Car {{car}}</a>
+            </router-link>
+        </ul>
+    </div>
 </template>
 
 <script>
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import VueRouter from 'vue-router'
 import Home from './pages/Home'
 import Cars from './pages/Cars'
+import Car from './pages/Car'
 
 export default new VueRouter({
     routes: [
@@ -12,6 +13,11 @@ export default new VueRouter({
         {
             path: '/cars', //http://localhost:8080/cars
             component: Cars
+        },
+
+        {
+            path: '/car/:id',
+            component: Car
         },
     ],
     mode: 'history' //Избавляемся от # в адресе
