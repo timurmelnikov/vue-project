59c20699d475b93fd2742dbb3a3afb5d8d57fe2a
Timur Melnikov <melnikovt@gmail.com>
2019-02-26 11:25:32 +0200

    Динамические роуты

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,18 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav">
-                    <!--<li class="nav-item">-->
-                    <!--<router-link class="nav-link" to="/">Home</router-link>-->
-                    <!--</li>-->
-                    <!--<li class="nav-item active">-->
-                    <!--<router-link class="nav-link" to="/cars">Cars</router-link>-->
-                    <!--</li>-->
                     <router-link tag="li" class="nav-item" exact to="/" active-class="active">
                         <a class="nav-link">Home</a>
                     </router-link>
                     <router-link tag="li" class="nav-item" to="/cars" active-class="active">
                         <a class="nav-link">Cars</a>
+                    </router-link>
+
+                    <router-link tag="li" class="nav-item" to="/car/3" active-class="active">
+                        <a class="nav-link">Car 3</a>
+                    </router-link>
+                    <router-link tag="li" class="nav-item" to="/car/4" active-class="active">
+                        <a class="nav-link">Car 4</a>
                     </router-link>
                 </ul>
             </div>
