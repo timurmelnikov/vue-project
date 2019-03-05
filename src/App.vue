46a66ecf2d518d0945fceaf7921f019e68d93270
Timur Melnikov <melnikovt@gmail.com>
2019-03-05 12:28:39 +0200

    Создание объектов POST

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
-    <div id="container">
-        <h1>wwwww</h1>
+    <div id="container зе-2">
+        <div class="form-group">
+            <label for="name">Car name</label>
+            <input id="name" class="form-control" type="text" v-model.trim="carName">
+        </div>
+        <div class="form-group">
+            <label for="year">Car name</label>
+            <input id="year" class="form-control" type="text" v-model.number="carYear">
+        </div>
+        <button class="btn btn-success" @click="createCar">Create</button>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'app',
         data() {
             return {
-                msg: 'Welcome to Your Vue.js App'
+                carName: '',
+                carYear: 2018
+            }
+        },
+        methods: {
+            createCar() {
+                const car = {
+                    name: this.carName,
+                    year: this.carYear
+                }
+                this.$http.post('http://localhost:3000/cars', car)
+                    .then(response => {
+                        //console.log(response)
+                        return response.json()
+                    })
+                    .then(newCar => {
+                        console.log(newCar)
+                    })
             }
         }
     }
