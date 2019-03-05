b8b6fbf028f16c8b848705de65c6ffc960931d00
Timur Melnikov <melnikovt@gmail.com>
2019-03-05 12:07:52 +0200

    Настройка приложения

--- src/main.js
+++ src/main.js
@@ -1,16 +1,11 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-import router from './routes'
-
-Vue.use(VueRouter)
 
 Vue.use(VueResource)
 
 new Vue({
     el: '#app',
     render: h => h(App),
-    router: router
 
 })
