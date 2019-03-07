74b117ffc15372c974e87ee5f5d7185353a25a28
Timur Melnikov <melnikovt@gmail.com>
2019-03-07 13:16:52 +0200

    Интерсепторы

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import App from './App.vue'
 Vue.use(VueResource)
 Vue.http.options.root = 'http://localhost:3000/'
 
+Vue.http.interceptors.push(request => {
+    request.headers.set('Auth', 'RAND TOKEN ' + Math.random());
+});
+
 new Vue({
     el: '#app',
     render: h => h(App),
