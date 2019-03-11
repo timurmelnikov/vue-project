90c0062d072334ef089bfdbe1cdd0541b9acede8
Timur Melnikov <melnikovt@gmail.com>
2019-03-11 09:53:18 +0200

    Установка vuex

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,24 @@
 <template>
-    <div id="container зе-2">
-        <div class="form-group">
-            <label for="name">Car name</label>
-            <input id="name" class="form-control" type="text" v-model.trim="carName">
-        </div>
-        <div class="form-group">
-            <label for="year">Car name</label>
-            <input id="year" class="form-control" type="text" v-model.number="carYear">
-        </div>
-        <button class="btn btn-success" @click="createCar">Create</button>
-        <button class="btn btn-primary" @click="loadCars">Load Cars</button>
-        <hr>
-        <ul class="list-group">
-            <li c
-                lass="list-group-item"
-                v-for="car of cars"
-                :key="car.id"
-            >
-                <strong>{{car.name}}</strong> - {{car.year}}
-            </li>
-        </ul>
+    <div class="container text-center pt-5">
+        <app-counter :counter="counter"></app-counter>
+        <app-actions @counterUpdated="counter += $event"></app-actions>
+
     </div>
 </template>
 
 <script>
+    import Counter from './Counter'
+    import Actions from './Actions'
+
     export default {
         data() {
             return {
-                carName: '',
-                carYear: 2018,
-                cars: [],
-                resource: null
+                counter: 0
             }
         },
-        methods: {
-            createCar() {
-                const car = {
-                    name: this.carName,
-                    year: this.carYear
-                }
-                this.resource.save({}, car)
-            },
-            loadCars() {
-                this.resource.get()
-                    .then(response => {
-                        return response.json()
-                    })
-                    .then(cars => {
-                        this.cars = cars
-                    })
-            }
-        },
-        created() {
-            this.resource = this.$resource('cars')
+        components: {
+            appCounter: Counter,
+            appActions: Actions
         }
     }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,10 @@
 import Vue from 'vue'
-import VueResource from 'vue-resource'
 import App from './App.vue'
-
-Vue.use(VueResource)
-Vue.http.options.root = 'http://localhost:3000/'
-
-Vue.http.interceptors.push(request => {
-    request.headers.set('Auth', 'RAND TOKEN ' + Math.random());
-});
+import store from './store'
 
 new Vue({
     el: '#app',
+    store,
     render: h => h(App),
 
 })
