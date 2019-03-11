e6fb0f0d8bdb98f7b07457757c69819558a5f3ea
Timur Melnikov <melnikovt@gmail.com>
2019-03-11 10:47:56 +0200

    Использование getters

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="container text-center pt-5">
         <app-counter></app-counter>
+        <app-second-counter></app-second-counter>
+        <hr>
         <app-actions></app-actions>
 
     </div>
@@ -8,11 +10,13 @@
 
 <script>
     import Counter from './Counter'
+    import SecondCounter from './SecondCounter'
     import Actions from './Actions'
 
     export default {
         components: {
             appCounter: Counter,
+            appSecondCounter: SecondCounter,
             appActions: Actions
         }
     }
